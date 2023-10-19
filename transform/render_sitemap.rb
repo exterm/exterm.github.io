731f3fb8bc24484ae53757747ecb0dcf5cb4227d
Philip Mueller <mail@philip.in-aachen.net>
2023-10-18 22:41:09 -0400

    get mtime for sitemap from git

--- transform/render_sitemap.rb
+++ transform/render_sitemap.rb
@@ -6,18 +6,22 @@ require "date"
 DOMAIN = "https://simplexity.quest"
 TARGET = "../generated/public/sitemap.xml"
 
+def git_mtime(path)
+  DateTime.parse(%x(git log -1 --pretty="format:%ci" #{path}).strip)
+end
+
 Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
   xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") {
     xml.url {
       xml.loc "https://simplexity.quest/"
       # last modification for the index is the latest of all posts
-      latest_post = Dir.glob("../content/posts/**/*.md").map { |path| File.mtime(path).to_datetime }.max
+      latest_post = Dir.glob("../content/posts/**/*.md").map { |path| git_mtime(path) }.max
       xml.lastmod latest_post.iso8601
     }
     Dir.glob("../content/posts/*.md").each do |path|
       xml.url {
         xml.loc "#{DOMAIN}/posts/#{File.basename(path, ".md")}.html"
-        xml.lastmod File.mtime(path).to_datetime.iso8601
+        xml.lastmod git_mtime(path).iso8601
       }
     end
   }
