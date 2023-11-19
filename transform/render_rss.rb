329b39dda27f6086c6e885677bbdd1b9e740ce50
Philip Mueller <mail@philip.in-aachen.net>
2023-11-19 00:36:31 -0500

    fix rss format
    move even more deliberately and fix more things

--- transform/render_rss.rb
+++ transform/render_rss.rb
@@ -1,11 +1,14 @@
 require "nokogiri"
 require "date"
 require "pathname"
+require "json"
 
 # render an RSS feed from all post sources (markdown files)
 
 input_dir = Pathname.new(ARGV[0])
 output_file = Pathname.new(ARGV[1])
+
+all_mds = Pathname.glob(input_dir + "*.md")
 
 DOMAIN = "https://simplexity.quest"
 TITLE = "Simplexity Quest"
@@ -14,20 +17,27 @@ def git_mtime(path)
   DateTime.parse(%x(git log -1 --pretty="format:%ci" #{path}).strip)
 end
 
-all_mds = Pathname.glob(input_dir + "*.md")
+def extract_title(path)
+  here = Pathname.new(__FILE__).dirname.expand_path
+  JSON.parse(%x(pandoc --template=#{here + "pandoc/templates/metadata.tpl"} #{path}))["title"]
+end
 
 Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
-  xml.rss(version: "2.0") {
+  xml.rss(version: "2.0", "xmlns:atom" => "http://www.w3.org/2005/Atom") {
     xml.channel {
       xml.title TITLE
       xml.link(DOMAIN + "/")
+      xml["atom"].link href: DOMAIN + "/rss.xml", rel: "self", type: "application/rss+xml"
       xml.description TITLE
       latest_post = all_mds.map { |path| git_mtime(path) }.max
-      xml.pubDate latest_post.iso8601
+      xml.pubDate latest_post.rfc822
       all_mds.each do |path|
         xml.item {
-          xml.link "#{DOMAIN}/posts/#{path.basename(".md")}.html"
-          xml.pubDate git_mtime(path).iso8601
+          xml.title extract_title(path)
+          link = "#{DOMAIN}/posts/#{path.basename(".md")}.html"
+          xml.link link
+          xml.guid link
+          xml.pubDate git_mtime(path).rfc822
         }
       end
     }
