a21765fab725fe32a05b887832adf47fc7c79b0a
Philip Mueller <mail@philip.in-aachen.net>
2023-11-19 00:07:39 -0500

    move deliberately and fix things

--- transform/render_rss.rb
+++ transform/render_rss.rb
@@ -1,15 +1,20 @@
 require "nokogiri"
 require "date"
+require "pathname"
 
 # render an RSS feed from all post sources (markdown files)
 
+input_dir = Pathname.new(ARGV[0])
+output_file = Pathname.new(ARGV[1])
+
 DOMAIN = "https://simplexity.quest"
-TARGET = "../generated/public/rss.xml"
 TITLE = "Simplexity Quest"
 
 def git_mtime(path)
   DateTime.parse(%x(git log -1 --pretty="format:%ci" #{path}).strip)
 end
+
+all_mds = Pathname.glob(input_dir + "*.md")
 
 Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
   xml.rss(version: "2.0") {
@@ -17,16 +22,16 @@ Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
       xml.title TITLE
       xml.link(DOMAIN + "/")
       xml.description TITLE
-      latest_post = Dir.glob("../content/posts/**/*.md").map { |path| git_mtime(path) }.max
+      latest_post = all_mds.map { |path| git_mtime(path) }.max
       xml.pubDate latest_post.iso8601
-      Dir.glob("../content/posts/*.md").each do |path|
+      all_mds.each do |path|
         xml.item {
-          xml.link "#{DOMAIN}/posts/#{File.basename(path, ".md")}.html"
+          xml.link "#{DOMAIN}/posts/#{path.basename(".md")}.html"
           xml.pubDate git_mtime(path).iso8601
         }
       end
     }
   }
 end.to_xml.tap do |xml|
-  File.write(TARGET, xml)
+  File.write(output_file, xml)
 end
--- transform/render_sitemap.rb
+++ transform/render_sitemap.rb
@@ -18,7 +18,7 @@ all_mds = Pathname.glob(input_dir + "*.md")
 Nokogiri::XML::Builder.new(encoding: "UTF-8") do |xml|
   xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") {
     xml.url {
-      xml.loc DOMAIN
+      xml.loc(DOMAIN + "/")
       # last modification for the index is the latest of all posts
       latest_post = all_mds.map { |path| git_mtime(path) }.max
       xml.lastmod latest_post.iso8601
